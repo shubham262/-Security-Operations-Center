a877d010beaf98a4888ba21d01cbd5a904a9f6f2
Shubham Manohar <shubhammanohar20@gmail.com>
2026-05-07 13:49:37 +0530

    backend initial setup

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { handleBetterAuth } from "./src/config/auth.js";
+import { toNodeHandler } from "better-auth/node";
+const app = express();
+
+const auth = await handleBetterAuth();
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+app.use(express.json());
+app.use("/api/auth", toNodeHandler(auth));
+app.listen(3001, () => {
+	console.log("Server started at port 3001");
+});
---
+++ backend/src/config/auth.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { betterAuth } from "better-auth";
+import { handleMongoDBConnection } from "./index.js";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+let auth = null;
+export const handleBetterAuth = async () => {
+	if (auth) return auth;
+	const { db } = await handleMongoDBConnection();
+	auth = betterAuth({
+		database: mongodbAdapter(db),
+		emailAndPassword: {
+			enabled: true,
+		},
+		secret: process.env.BETTER_AUTH_SECRET,
+		baseURL: process.env.BETTER_AUTH_URL,
+		trustedOrigins: ["http://localhost:3000"],
+	});
+	return auth;
+};
