7bb0f72f30f486122a7aa0ef4e3c259e590628eb
Shubham Manohar <shubhammanohar20@gmail.com>
2026-05-07 15:54:15 +0530

    seeding completed

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -3,6 +3,7 @@ dotenv.config();
 import { betterAuth } from "better-auth";
 import { handleMongoDBConnection } from "./index.js";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { seedInitialAlerts } from "../helpers/seedAlert.js";
 
 let auth = null;
 export const handleBetterAuth = async () => {
@@ -16,6 +17,15 @@ export const handleBetterAuth = async () => {
 		secret: process.env.BETTER_AUTH_SECRET,
 		baseURL: process.env.BETTER_AUTH_URL,
 		trustedOrigins: ["http://localhost:3000"],
+		databaseHooks: {
+			user: {
+				create: {
+					after: async (user) => {
+						await seedInitialAlerts(user?.id);
+					},
+				},
+			},
+		},
 	});
 	return auth;
 };
